48caee87edba7daec4c63a3c40e4508fd910ee52
Felix Hallenberg <feqwin@gmail.com>
2025-10-20 14:21:49 +0300

    Update conform config

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,25 +1,19 @@
 return {
   "stevearc/conform.nvim",
   opts = {
-    -- These options will be passed to conform.format()
     timeout_ms = 500,
     lsp_fallback = true,
     formatters_by_ft = {
       lua = { "stylua" },
-      -- Conform will run multiple formatters sequentially
       python = { "isort", "black" },
-      -- Use a sub-list to run only the first available formatter
-      typescript = { { "prettierd", "prettier" } },
-      typescriptreact = {
-        { "eslintd",   "eslint" },
-        { "prettierd", "prettier" },
-      },
-      json = { "fixjson", "prettier" }
+      typescript = { "prettierd", "prettier" },
+      typescriptreact = { "eslintd", "eslint", "prettierd", "prettier" },
+      json = { "fixjson", "prettier" },
     },
     format_on_save = {
-      -- These options will be passed to conform.format()
       timeout_ms = 500,
       lsp_fallback = true,
     },
+    stop_after_first = true,
   },
 }
